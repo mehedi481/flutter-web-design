1f8a7b6af1d9b7cf1047d808a159443d74058ec1
Mehedi Hasan <mehedihasanmax95@gmail.com>
2022-09-12 21:03:42 +0600

    profile pic & title added in homePage right side

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,9 +1,9 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_web_design/profile_title.dart';
 import 'package:google_fonts/google_fonts.dart';
-
 import 'header.dart';
+import 'profile_image.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -125,6 +125,73 @@ class HomePage extends StatelessWidget {
                         )
                       ]),
                     ),
+                    Container(
+                      height: 600.0,
+                      width: MediaQuery.of(context).size.width * 0.55,
+                      color: Colors.white,
+                      child: Stack(
+                        clipBehavior: Clip.none,
+                        children: const [
+                          ProfileImage(
+                            top: 140.0,
+                            left: 90.0,
+                            diameter: 200.0,
+                            image:
+                                "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+                          ),
+                          ProfileImage(
+                            top: 160.0,
+                            left: 310.0,
+                            diameter: 100.0,
+                            image:
+                                "https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+                          ),
+                          ProfileImage(
+                            top: 360.0,
+                            left: 90.0,
+                            diameter: 170.0,
+                            image:
+                                "https://images.unsplash.com/photo-1593104547489-5cfb3839a3b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=853&q=80",
+                          ),
+                          ProfileImage(
+                            top: 275.0,
+                            left: 280.0,
+                            diameter: 280.0,
+                            image:
+                                "https://images.unsplash.com/photo-1554727242-741c14fa561c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+                          ),
+                          
+                          ProfileTitle(
+                            top: 140,
+                            left: 50,
+                            title: "I am Gonna give you Color theory",
+                            subTitle: "Creator, Designer",
+                            factor: 0.5,
+                          ),
+                          ProfileTitle(
+                            top: 160,
+                            left: 380,
+                            title: "I am Gonna give you Color theory",
+                            subTitle: "Creator, Designer",
+                            factor: 0.5,
+                          ),
+                          ProfileTitle(
+                            top: 360,
+                            left: 50,
+                            title: "I am Gonna give you Color theory",
+                            subTitle: "Creator, Designer",
+                            factor: 0.5,
+                          ),
+                          ProfileTitle(
+                            top: 275,
+                            left: 450,
+                            title: "I am Gonna give you Color theory",
+                            subTitle: "Creator, Designer",
+                            factor: 0.5,
+                          ),
+                        ],
+                      ),
+                    )
                   ],
                 ),
                 const Header(),
---
+++ lib/profile_image.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class ProfileImage extends StatelessWidget {
+  const ProfileImage(
+      {Key? key,
+      required this.top,
+      required this.left,
+      required this.diameter,
+      required this.image})
+      : super(key: key);
+  final double top;
+  final double left;
+  final double diameter;
+  final String image;
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: top,
+      left: left,
+      child: SizedBox(
+        height: diameter,
+        width: diameter,
+        child: ClipRRect(
+          child: Image.network(
+            image,
+            fit: BoxFit.cover,
+          ),
+          borderRadius: BorderRadius.circular(300.0),
+        ),
+      ),
+    );
+  }
+}
